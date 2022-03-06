1bf61fc67327ecfdabb3c241e6abd45ff3fedbde
Asmahan <asmahanhmohamed@gmail.com>
2022-03-06 18:39:32 +0000

    List JS.

--- index.js
+++ index.js
@@ -13,24 +13,31 @@ createNewList.addEventlistener('click', createList())
 //function to create a new list onclick.
 function createList() {
 
-    //create new div element.
+    //create outer div to contain list.
     const newList = document.createElement('div')
-    newList.setAttribute("class", "list-input")
-    newList.innerHTML=(inputTitle, inputBtn)
+    newList.setAttribute("class", "list-container")
+   
+
+    //create div for input and button element.
+    const inputDiv = document.createElement('div')
+    inputTitle.setAttribute("class", "list-input")
+    newList.appendChild(inputDiv)
+
 
     //create input element.
     const inputTitle = document.createElement('input')
     inputTitle.setAttribute("type", "text")
     inputTitle.setAttribute("placeholder", "Add new title.")
+   
 
-    newList.appendChild(inputTitle)
+    inputDiv.appendChild(inputTitle)
 
     //create button to submit input.
     const inputBtn = document.createElement('button')
     inputBtn.setAttribute("id", "#add-title")
     inputBtn.setAttribute("value", "Add")
 
-    newList.appendChild(inputBtn)
+    inputDiv.appendChild(inputBtn)
     
     //append the new list to tv button.
     const element = document.querySelector('.tv')
