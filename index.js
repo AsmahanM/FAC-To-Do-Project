6b15c5b7d74ad80b8b303c656b537743899c97c4
Asmahan <asmahanhmohamed@gmail.com>
2022-03-06 19:07:23 +0000

    List JS.

--- index.js
+++ index.js
@@ -15,27 +15,33 @@ function createList() {
 
     //create outer div to contain list.
     const newList = document.createElement('div')
-    newList.setAttribute("class", "list-container")
+    // newList.setAttribute("class", "list-container")
+    newList.class="list-container"
    
 
     //create div for input and button element.
     const inputDiv = document.createElement('div')
-    inputTitle.setAttribute("class", "list-input")
+    // inputTitle.setAttribute("class", "list-input")
+    inputDiv.class="list-input"
     newList.appendChild(inputDiv)
-
+    
 
     //create input element.
     const inputTitle = document.createElement('input')
-    inputTitle.setAttribute("type", "text")
-    inputTitle.setAttribute("placeholder", "Add new title.")
+    inputTitle.type="text"
+    inputTitle.placeholder="Add new title."
+    // inputTitle.setAttribute("type", "text")
+    // inputTitle.setAttribute("placeholder", "Add new title.")
    
 
     inputDiv.appendChild(inputTitle)
 
     //create button to submit input.
     const inputBtn = document.createElement('button')
-    inputBtn.setAttribute("id", "#add-title")
-    inputBtn.setAttribute("value", "Add")
+    inputBtn.id="add-title"
+    inputBtn.value="Add"
+    // inputBtn.setAttribute("id", "#add-title")
+    // inputBtn.setAttribute("value", "Add")
 
     inputDiv.appendChild(inputBtn)
     
