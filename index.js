9cd0e51255f12529ec9fc0c85b0dc5b023e39cff
Asmahan <asmahanhmohamed@gmail.com>
2022-03-06 19:26:50 +0000

    More refactoring.

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ function createList() {
 
     //create outer div to contain list.
     const newList = document.createElement('div')
-    // newList.setAttribute("class", "list-container")
-    newList.class="list-container"
+    newList.classList.add("list-container")
    
 
     //create inner div for input and button element.
     const inputDiv = document.createElement('div')
-    // inputTitle.setAttribute("class", "list-input")
     inputDiv.classList.add("list-input")
+    
     newList.appendChild(inputDiv)
     
 
@@ -25,22 +24,19 @@ function createList() {
     inputTitle.type="text"
     inputTitle.placeholder="Add new title."
     inputTitle.classList.add("list-input")
-    // inputTitle.setAttribute("type", "text")
-    // inputTitle.setAttribute("placeholder", "Add new title.")
    
-
     inputDiv.appendChild(inputTitle)
+
 
     //create button to submit input.
     const inputBtn = document.createElement('button')
     inputBtn.id="add-title"
     inputBtn.innerHTML="Add"
     inputBtn.classList.add("list-input")
-    // inputBtn.setAttribute("id", "#add-title")
-    // inputBtn.setAttribute("value", "Add")
 
     inputDiv.appendChild(inputBtn)
     
+
     //append the new list to tv button.
     const element = document.querySelector('.tv')
     element.appendChild(newList)
